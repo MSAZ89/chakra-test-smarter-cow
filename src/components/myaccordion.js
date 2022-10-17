5476f5f83893306391e83f52f19298b15241eaa1
MSAZ89 <michaelsimonaz@gmail.com>
2022-10-17 10:12:47 -0700

    initial

---
+++ src/components/myaccordion.js
@@ -0,0 +1,58 @@
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Box,
+  } from '@chakra-ui/react'
+
+import { useState } from 'react'
+
+
+export default function MyAccordion (){
+
+const [disableNd, setDisableNd] = useState(false);
+
+    return(
+        <>
+            <button onClick={() => setDisableNd(!disableNd)}>toggle disabled</button>
+            <br/><br/>
+            <Accordion defaultIndex={[0]} allowMultiple>
+                <AccordionItem isDisabled={disableNd}>
+                    <h2>
+                    <AccordionButton>
+                        <Box flex='1' textAlign='left'>
+                        Section 1 title
+                        </Box>
+                        <AccordionIcon />
+                    </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat.
+                    </AccordionPanel>
+                </AccordionItem>
+
+                <AccordionItem>
+                    <h2>
+                    <AccordionButton>
+                        <Box flex='1' textAlign='left'>
+                        Section 2 title
+                        </Box>
+                        <AccordionIcon />
+                    </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat.
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+        </>
+    )
+}
